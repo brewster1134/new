52d03f45bef2b21e6ff3cef5f908428a8542c651
Ryan Brewster <brewster1134+tower@gmail.com>
2015-03-14 19:59:45 -0700

    1.2.3 * foo * bar

--- lib/new.rb
+++ lib/new.rb
@@ -1,6 +1,8 @@
 require 'active_support/core_ext/hash/deep_merge'
 require 'yaml'
 
+puts "TEST"
+
 # Allow true/false to respond to Boolean class
 module Boolean; end
 class TrueClass; include Boolean; end
